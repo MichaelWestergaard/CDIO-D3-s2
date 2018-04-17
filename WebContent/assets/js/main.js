1ee1cc99713708c27ad435f3ca5924a051485a9e
Michael Westergaard - Laptop <michaelwestergaard@hotmail.dk>
2018-04-17 11:20:46 +0200

    Fixes

--- WebContent/assets/js/main.js
+++ WebContent/assets/js/main.js
@@ -1,7 +1,5 @@
-$(document).ready(function(){
-	$(".content-container").load("Brugerliste.html");
-});
-
-$('#options').on('change', function() {
-	alert("gg");
+$(document).ready(function(){	
+	$('#homeBtn').on('click', function(){
+		$(".content-container").load("Brugerliste.html",function(){}).hide().fadeIn();
+	});
 });
