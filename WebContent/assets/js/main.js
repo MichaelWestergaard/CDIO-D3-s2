34e7334e4e97134affa8b3153ca08b7f04a2f141
Michael Westergaard <michaelwestergaard@hotmail.dk>
2018-04-23 19:30:57 +0200

    Fixed

--- WebContent/assets/js/main.js
+++ WebContent/assets/js/main.js
@@ -1,5 +1,32 @@
 $(document).ready(function(){	
 	$('#homeBtn').on('click', function(){
-		$(".content-container").load("Brugerliste.html",function(){}).hide().fadeIn();
+		console.log("home");
+		$(".content-container").load("Brugerliste.html");
+	});
+	$('#createBtn').on('click', function(){
+		console.log("create");
+		$(".content-container").load("opretBruger.html");
+	});
+	
+	//Brugerliste
+	$('#options').on('change', function() {
+		
+		if (this.value == "deleteUser") {
+			console.log("test");
+		}
+		
+		if (this.value == "updateUser") {
+
+			console.log("test1");
+		$(".content-container").load('updateUser.html');
+		}
+		
+		if (this.value == "resetPass") {
+
+			console.log("test3");
+		$(".content-container").load('resetPassword.html');
+		}
+		
 	});
 });
+
