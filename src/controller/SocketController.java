834f88529f854942ea8b0bfb4088c6a86a0c980b
Michael Westergaard - Laptop <michaelwestergaard@hotmail.dk>
2018-06-08 11:55:11 +0200

    CompleteProcedure tilføjet

--- src/controller/SocketController.java
+++ src/controller/SocketController.java
@@ -30,7 +30,7 @@ public class SocketController implements Runnable {
 
 	public void init() {
 		try {
-			socket = new Socket("169.254.2.3", 8000);
+			socket = new Socket("169.254.2.2", 8000);
 		} catch (UnknownHostException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
@@ -87,7 +87,7 @@ public class SocketController implements Runnable {
 						sendMessage("RM20 8 \"Indtast ID igen:\" \"\" \"&3\"");
 					}
 				} else {
-					sendMessage("RM20 8 \"Forkert ID! Prøv igen\" \"\" \"&3\"");
+					sendMessage("RM20 8 \"Forkert ID! Proev igen\" \"\" \"&3\"");
 				}
 			}			
 		} catch (IOException e) {
@@ -107,6 +107,10 @@ public class SocketController implements Runnable {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+	
+	public void completeProcedure() {
+		loginProcedure();
 	}
 	
 }
