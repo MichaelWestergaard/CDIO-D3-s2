b5b158a6491baaff8eb41595bc63429c4cd99a77
s163989 <s163989@vlan627.mekanik.router.net.dtu.dk>
2018-06-11 13:15:06 +0200

    michaels fix

--- src/datalag/MySQLConnector.java
+++ src/datalag/MySQLConnector.java
@@ -7,7 +7,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-
 public class MySQLConnector {
 	private static final String DRIVER_CLASS = "com.mysql.jdbc.Driver";
 //	private static final String DRIVER_CLASS = "com.mysql.cj.jdbc.Driver";
@@ -15,45 +14,27 @@ public class MySQLConnector {
 //	private static final String URL = "jdbc:mysql://mysql25.unoeuro.com/michaelwestergaard_dk_db?useSSL=false&serverTimezone=UTC";//&zeroDateTimeBehavior=convertToNull
 	private static final String USER = "michaelwest_dk";
 	private static final String PASSWORD = "68wukovuzovi";
-
-	private Statement statement = null;
-	private PreparedStatement preparedStatement = null;
-
-	public MySQLConnector() throws ClassNotFoundException {
-		Class.forName(DRIVER_CLASS);
-	}
-
-	private static Connection startConnection() throws SQLException {
-		Connection connection = (Connection) DriverManager.getConnection(URL, USER, PASSWORD);
-		return connection;
-	}
-
-	public static Connection getConnection() throws SQLException {
-		return startConnection();
-	}
-	
-	
 	
 	private static MySQLConnector connector;
-	private Connection conn;
+	private Connection connection;
 	
+	private MySQLConnector() throws ClassNotFoundException, SQLException {
+		//initialize connection!
+		Class.forName(DRIVER_CLASS);
+		connection = (Connection) DriverManager.getConnection(URL, USER, PASSWORD);
+	}
 	
-	
-	public static MySQLConnector getInstance() {
-		try {
-			if (connector== null && getConnection() == null) { // and check connection!
-				connector = new MySQLConnector();
-			}
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+	public static MySQLConnector getInstance() throws ClassNotFoundException, SQLException {
+		if (connector == null || connector.getConnection() == null) {
+			connector = new MySQLConnector();
 		}
 		return connector;
 	}
 	
+	private Connection getConnection() {
+		return connection;
+	}
+
 	PreparedStatement getStatement(){
 		//TODO 
 	}
@@ -66,9 +47,5 @@ public class MySQLConnector {
 	void doUpdate(Statement stmnt) {
 		
 	}
-	
 
-	
-	
-	
 }
