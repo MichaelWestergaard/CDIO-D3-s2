88b7769cb8589977e6a1beeeab64d45be1215b85
eriks <eriks@DESKTOP-OOBN7VU.lan>
2018-06-12 00:11:02 +0200

    trycatch flyttet til controller

--- src/controller/IngredientController.java
+++ src/controller/IngredientController.java
@@ -23,17 +23,29 @@ public class IngredientController extends ResponseHandler{
 	private IngredientDAO ingDAO = new IngredientDAO();
 	private IngBatchDAO ingBatchDAO = new IngBatchDAO();
 	
-	public String getIngredient(int ingredientID) throws ClassNotFoundException, SQLException {
-		return createResponse("success", 1, new Gson().toJson(ingDAO.read(ingredientID)));
+	public String getIngredient(int ingredientID) {
+		try {
+			return createResponse("success", 1, new Gson().toJson(ingDAO.read(ingredientID)));
+		} catch (ClassNotFoundException e) {
+			return createResponse("error", 0, e.getMessage());
+		} catch (SQLException e) {
+			return createResponse("error", e.getErrorCode(), e.getMessage());
+		}
 	}
 	
-	public String createIngredient(int ingredientID, String ingredientName) throws ClassNotFoundException, SQLException {
+	public String createIngredient(int ingredientID, String ingredientName) {
 		if(ingredientID >= 1 && ingredientID <= 99999999) {
 			if(ingredientName.length() >= 2 && ingredientName.length() <= 20) {
 				IngredientDTO ingredient = new IngredientDTO(ingredientID, ingredientName);
 				
-				if(ingDAO.create(ingredient)) {			
-					return createResponse("success", 1, "Råvaren \"" + ingredientName + "\" blev oprettet");
+				try {
+					if(ingDAO.create(ingredient)) {			
+						return createResponse("success", 1, "Råvaren \"" + ingredientName + "\" blev oprettet");
+					}
+				} catch (ClassNotFoundException e) {
+					return createResponse("error", 0, e.getMessage());
+				} catch (SQLException e) {
+					return createResponse("error", e.getErrorCode(), e.getMessage());
 				}
 			} else {
 				return createResponse("error", 0, "Råvare navnet skal være 2-20 tegn!");
@@ -45,61 +57,94 @@ public class IngredientController extends ResponseHandler{
 		return createResponse("error", 0, "Kunne ikke oprette Råvare");
 	}
 
-	public String editIngredient(int ingredientID, String ingredientName) throws ClassNotFoundException, SQLException {
+	public String editIngredient(int ingredientID, String ingredientName) {
 		if(ingredientName.length() < 2 || ingredientName.length() > 20) {
 			return createResponse("error", 0, "Råvarenavnet skal være mellem 2 - 20 tegn");
 		}
 		
 		IngredientDTO ingredient = new IngredientDTO(ingredientID, ingredientName);
 		
-		if(ingDAO.update(ingredient)) {
-			return createResponse("success", 1, "Råvaren blev opdateret");
-		} else {
-			return createResponse("error", 0, "Råvaren kunne ikke opdateres");
+		try {
+			if(ingDAO.update(ingredient)) {
+				return createResponse("success", 1, "Råvaren blev opdateret");
+			} else {
+				return createResponse("error", 0, "Råvaren kunne ikke opdateres");
+			}
+		} catch (ClassNotFoundException e) {
+			return createResponse("error", 0, e.getMessage());
+		} catch (SQLException e) {
+			return createResponse("error", e.getErrorCode(), e.getMessage());
 		}
 	}
 
-	public String getIngBatch(int ingBatchID) throws ClassNotFoundException, SQLException {
-		return createResponse("success", 1, new Gson().toJson(ingBatchDAO.read(ingBatchID)));
+	public String getIngBatch(int ingBatchID) {
+		try {
+			return createResponse("success", 1, new Gson().toJson(ingBatchDAO.read(ingBatchID)));
+		} catch (ClassNotFoundException e) {
+			return createResponse("error", 0, e.getMessage());
+		} catch (SQLException e) {
+			return createResponse("error", e.getErrorCode(), e.getMessage());
+		}
 	}
 	
-	public String createIngBatch(int ingBatchID, int ingredientID, double amount, String supplier) throws ClassNotFoundException, SQLException {
-		if(ingBatchID >= 1 && ingBatchID <= 99999999) {
-			if(ingredientID >= 1 && ingredientID <= 99999999) {
-				if(supplier.length() < 2 || supplier.length() > 20) {
-					return createResponse("error", 0, "Leverandørnavnet skal være mellem 2 - 20 tegn");
-				} else {
-					IngBatchDTO ingBatch = new IngBatchDTO(ingBatchID, ingredientID, amount, "", supplier);
-					
+	public String createIngBatch(int ingBatchID, int ingredientID, double amount, String supplier) {
+		if(ingredientID >= 1 && ingredientID <= 99999999) {
+			if(supplier.length() < 2 || supplier.length() > 20) {
+				return createResponse("error", 0, "Leverandørnavnet skal være mellem 2 - 20 tegn");
+			} else {
+				IngBatchDTO ingBatch = new IngBatchDTO(ingBatchID, ingredientID, amount, "", supplier);
+				
+				try {
 					if(ingBatchDAO.create(ingBatch)) {
 						return createResponse("success", 1, "Råvarebatchen med råvaren \"" + ingDAO.read(ingredientID).getIngredientName() + "\" blev oprettet");
 					}
+				} catch (ClassNotFoundException e) {
+					return createResponse("error", 0, e.getMessage());
+				} catch (SQLException e) {
+					return createResponse("error", e.getErrorCode(), e.getMessage());
 				}
-			} else {
-				return createResponse("error", 0, "Råvare ID skal være i mellem 1-99999999!");
 			}
 		} else {
-			return createResponse("error", 0, "Råvarebatch ID skal være i mellem 1-99999999!");
+			return createResponse("error", 0, "Råvare ID skal være i mellem 1-99999999!");
 		}
 		
 		return createResponse("error", 0, String.valueOf(amount));
 	}
 
-	public String getIngredientList() throws ClassNotFoundException, SQLException {
+	public String getIngredientList() {
 		String returnMsg = "";
-		
-		List<IngredientDTO> ingredients = ingDAO.list();
-		String json = new Gson().toJson(ingredients);
-		returnMsg = json;
+				
+		try {
+			List<IngredientDTO> ingredients;
+			ingredients = ingDAO.list();
+			String json = new Gson().toJson(ingredients);
+			returnMsg = json;
+		} catch (ClassNotFoundException e) {
+			return createResponse("error", 0, e.getMessage());
+		} catch (SQLException e) {
+			return createResponse("error", e.getErrorCode(), e.getMessage());
+		}		
 		
 		return returnMsg;
 	}
 
-	public String getIngBatchList() throws ClassNotFoundException, SQLException {
-		return createResponse("success", 1, new Gson().toJson(ingBatchDAO.list()));
+	public String getIngBatchList() {
+		try {
+			return createResponse("success", 1, new Gson().toJson(ingBatchDAO.list()));
+		} catch (ClassNotFoundException e) {
+			return createResponse("error", 0, e.getMessage());
+		} catch (SQLException e) {
+			return createResponse("error", e.getErrorCode(), e.getMessage());
+		}
 	}
 
-	public String getIngredientBatchesByIngredient(int ingredientID) throws ClassNotFoundException, SQLException {
-		return createResponse("success", 1, new Gson().toJson(ingBatchDAO.getIngredientBatchesByIngredient(ingredientID)));
+	public String getIngredientBatchesByIngredient(int ingredientID) {
+		try {
+			return createResponse("success", 1, new Gson().toJson(ingBatchDAO.getIngredientBatchesByIngredient(ingredientID)));
+		} catch (ClassNotFoundException e) {
+			return createResponse("error", 0, e.getMessage());
+		} catch (SQLException e) {
+			return createResponse("error", e.getErrorCode(), e.getMessage());
+		}
 	}
 }
--- src/rest/IngredientService.java
+++ src/rest/IngredientService.java
@@ -1,175 +1,72 @@
 package rest;
 
 import java.io.IOException;
-import java.sql.SQLException;
-import java.util.List;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
-import javax.ws.rs.core.Response;
-import javax.ws.rs.core.UriBuilder;
-
-import com.google.gson.Gson;
-
 import controller.IngredientController;
-import datalag.MySQLController;
-import datalag.ProductBatchDTO;
-import datalag.ResponseHandler;
-import datalag.IngredientDTO;
-import datalag.IngBatchDAO;
-import datalag.IngBatchDTO;
-import datalag.IngredientDAO;
-
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
 import javax.servlet.ServletContext;
-import javax.ws.rs.DELETE;
 import javax.ws.rs.FormParam;
 
 @Path("ingredient")
-public class IngredientService extends ResponseHandler {
-	//private IngredientController ctrl = new IngredientController();  //nyt
-	private IngredientController ingredientController;
-	private IngredientDAO ingDAO = new IngredientDAO();
-	private IngBatchDAO ingBatchDAO = new IngBatchDAO();
+public class IngredientService {
 	
-	public IngredientService() {
-		try {
-			mySQLController = new MySQLController();
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
+	private IngredientController ingredientController;
 	
 	//Råvareliste
 	@GET
 	@Path("getIngredientList")
 	public String getIngredientList() {		
-		try {
-			return ingredientController.getIngredientList();
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (SQLException e) {
-			return createResponse("error", e.getErrorCode(), e.getMessage());
-		}
-		
-		return null;
+		return ingredientController.getIngredientList();
 	}
 	
 	@GET
 	@Path("getIngredient")
 	public String getIngredient(@QueryParam("ingredientID") int ingredientID) {
-		try {
-			return ingredientController.getIngredient(ingredientID);
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		return null;	
+		return ingredientController.getIngredient(ingredientID);
 	}
 	
 	@POST
 	@Path("editIngredient")
 	public String editIngredient(@FormParam("ingredientID") int ingredientID, @FormParam("ingredientName") String ingredientName) {
-		try {
-			return ingredientController.editIngredient(ingredientID, ingredientName);
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		return null;			
+		return ingredientController.editIngredient(ingredientID, ingredientName);
 	}
 		
 	//Råvarebatchliste
 	@GET
 	@Path("getIngBatchList")
 	public String getIngBatchList() {
-		try {
-			ingredientController.getIngBatchList();
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (SQLException e) {
-			return createResponse("error", e.getErrorCode(), e.getMessage());
-		}
-		
-		return null;
-	}	
+		return ingredientController.getIngBatchList();
+	}
 
 	//Tilføj en Råvare
 	@POST
 	@Path("createIngredient")
 	public String createIngredient(@FormParam("ingredientID") int ingredientID, @FormParam("ingredientName") String ingredientName, @Context ServletContext context) throws IOException  {
-		try {
-			return ingredientController.createIngredient(ingredientID, ingredientName);
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return null;
+		return ingredientController.createIngredient(ingredientID, ingredientName);		
 	}
 	
 	@POST
 	@Path("createIngredientBatch")
 	public String createIngBatch(@FormParam("ingredientBatchID") int ingredientBatchID, @FormParam("ingredientID") int ingredientID, @FormParam("amount") double amount, @FormParam("supplier") String supplier, @Context ServletContext context) throws IOException  {
-		try {
-			ingredientController.createIngBatch(ingredientBatchID, ingredientID, amount, supplier);
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		return ingredientController.createIngBatch(ingredientBatchID, ingredientID, amount, supplier);
 		
-		return null;
 	}
 	
 	
 	@GET
 	@Path("getIngBatch")
 	public String getIngBatch(@QueryParam("ingBatchID") int ingBatchID) {
-		try {
-			return ingredientController.getIngBatch(ingBatchID);
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		return null;		
+		return ingredientController.getIngBatch(ingBatchID);
+				
 	}
 	
 	@GET
 	@Path("getIngredientBatchesByIngredient")
 	public String getIngredientBatchesByIngredient(@QueryParam("ingredientID") int ingredientID) {
-		try {
-			return ingredientController.getIngredientBatchesByIngredient(ingredientID);
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (SQLException e) {
-			return createResponse("error", e.getErrorCode(), e.getMessage());
-		}
-		
-		return null;
+		return ingredientController.getIngredientBatchesByIngredient(ingredientID);
 	}
 
 }
