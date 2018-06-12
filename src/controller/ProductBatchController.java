a6b23656a587a616fba38b1936616606b1197aa4
eriks <eriks@DESKTOP-OOBN7VU.lan>
2018-06-12 03:32:37 +0200

    ProdService og pbController done

--- src/controller/ProductBatchController.java
+++ src/controller/ProductBatchController.java
@@ -1,18 +1,24 @@
 package controller;
 
 import java.sql.SQLException;
-
 import com.google.gson.Gson;
 
 import datalag.ReceptDAO;
+import datalag.IngBatchDAO;
+import datalag.ProductBatchComponentDAO;
+import datalag.ProductBatchComponentDTO;
 import datalag.ProductBatchDAO;
 import datalag.ProductBatchDTO;
 import datalag.ResponseHandler;
+import datalag.UserDAO;
 
 public class ProductBatchController extends ResponseHandler {
 	
 	private ProductBatchDAO pbDAO = new ProductBatchDAO();
+	private ProductBatchComponentDAO pbcDAO = new ProductBatchComponentDAO();
 	private ReceptDAO receptDAO = new ReceptDAO();
+	private IngBatchDAO ingBatchDAO = new IngBatchDAO();
+	private UserDAO userDAO = new UserDAO();
 	
 	public String getProductBatch(int productBatchID) {		
 		try {
@@ -66,5 +72,90 @@ public class ProductBatchController extends ResponseHandler {
 		return createResponse("error", 0, "ProduktBatch kunne ikke oprettes");
 	}
 
+	public String getProductBatchComponent(int pbID) {
+		try {
+			return createResponse("success", 1, new Gson().toJson(pbcDAO.read(pbID)));
+		} catch (ClassNotFoundException e) {
+			return createResponse("error", 0, e.getMessage());
+		} catch (SQLException e) {
+			return createResponse("error", e.getErrorCode(), e.getMessage());
+		}
+	}
+
+	public String getProductBatchComponentList() {
+		try {
+			return createResponse("success", 1, new Gson().toJson(pbcDAO.list()));
+		} catch (ClassNotFoundException e) {
+			return createResponse("error", 0, e.getMessage());
+		} catch (SQLException e) {
+			return createResponse("error", e.getErrorCode(), e.getMessage());
+		}
+	}
+
+	public String createProductBatchComponent(int productBatchID, int ingredientBatchID, int operatorID, double netto, double tara) {
+		if(productBatchID < 1 || productBatchID > 99999999) {
+			return createResponse("error", 0, "ProduktBatch ID skal være mellem 1 - 99999999");
+		}
+		
+		if(ingredientBatchID < 1 || ingredientBatchID > 99999999) {
+			return createResponse("error", 0, "RåvareBatch ID skal være mellem 1 - 99999999");
+		}
+		
+		if(operatorID < 1 || operatorID > 99999999) {
+			return createResponse("error", 0, "Operatør ID skal være mellem 1 - 99999999");
+		}
+		
+		if(netto < 0.05 || netto > 20.0) {
+			return createResponse("error", 0, "Nettovægten skal være mellem 0.05 - 20.0");
+		}
+		
+		if(tara < 0.1 || tara > 10.0) {
+			return createResponse("error", 0, "Tara skal være mellem 0.1 - 10.0");
+		}
+		
+		try {
+			boolean productBatchFound = false;
+			boolean ingredientBatchFound = false;		
+			boolean userFound = false;
+			
+			if(pbcDAO.read(productBatchID) != null) {
+				return createResponse("error", 0, "ProdukBatchkomponenten eksisterer allerede");
+			}
+			
+			if(pbDAO.read(productBatchID) != null) {
+				productBatchFound = true;
+			}	
+			
+			if(ingBatchDAO.read(ingredientBatchID) != null) {
+				ingredientBatchFound = true;
+			}
+			
+			if(userDAO.read(operatorID) != null) {
+				userFound = true;
+			}
+			
+			if(productBatchFound && ingredientBatchFound && userFound) {
+				ProductBatchComponentDTO pbc = new ProductBatchComponentDTO(productBatchID, ingredientBatchID, operatorID, null, operatorID, null, netto, tara);
+				pbcDAO.create(pbc);
 	
+			} else if (!productBatchFound) {
+				return createResponse("error", 0, "productBatchID eksistere ikke");
+			} else if (!ingredientBatchFound) {
+				return createResponse("error", 0, "raavareBatchID eksistere ikke");
+			} else if (!userFound) {
+				return createResponse("error", 0, "operatorID eksistere ikke");
+			}
+			
+			if(pbcDAO.read(productBatchID) != null) {
+				return createResponse("success", 1, "ProduktBatchkomponenten med produktbatchen \"" + productBatchID + "\" blev oprettet");
+
+			} else {
+				return createResponse("error", 0, "Kunne ikke oprette produktbatchkomponenten");
+			}
+		} catch (ClassNotFoundException e) {
+			return createResponse("error", 0, e.getMessage());
+		} catch (SQLException e) {
+			return createResponse("error", e.getErrorCode(), e.getMessage());
+		} 
+	}
 }
--- src/rest/ProductService.java
+++ src/rest/ProductService.java
@@ -1,39 +1,18 @@
 package rest;
 
 import java.io.IOException;
-import java.sql.SQLException;
-import java.sql.Timestamp;
-import java.util.List;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
-import javax.ws.rs.core.Response;
-import javax.ws.rs.core.UriBuilder;
-
-import com.google.gson.Gson;
-
 import controller.ProductBatchController;
-import datalag.IngBatchDTO;
-import datalag.IngredientDTO;
-import datalag.MySQLController;
-import datalag.ProductBatchComponentDTO;
-import datalag.ProductBatchDTO;
-import datalag.ReceptComponentDTO;
-import datalag.ReceptDTO;
-import datalag.ResponseHandler;
-import datalag.UserDTO;
-
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
 import javax.servlet.ServletContext;
-import javax.ws.rs.DELETE;
 import javax.ws.rs.FormParam;
 
 
 	@Path("Product")
-	public class ProductService extends ResponseHandler{
+	public class ProductService {
 		
 		private ProductBatchController productBatchController;
 		
@@ -45,97 +24,20 @@ import javax.ws.rs.FormParam;
 		@GET
 		@Path("getProductBatchComponentList")
 		public String getProductBatchComponentList() {
-			try {
-				return createResponse("success", 1, new Gson().toJson(mySQLController.getProductBatchComponents()));
-			} catch (SQLException e) {
-				return createResponse("error", e.getErrorCode(), e.getMessage());
-			}
+			return productBatchController.getProductBatchComponentList();
 		}
 		
 		//Tilføj en ProduktBatchKomponent. Der skal laves en fejl besked
 		@POST
 		@Path("createProductBatchComponent")
 		public String createProductBatchComponent(@FormParam("productBatchID") int productBatchID, @FormParam("raavareBatchID") int raavareBatchID, @FormParam("operatorID") int operatorID, @FormParam("netto") double netto, @FormParam("tara") double tara, @Context ServletContext context) throws IOException  {
-			if(productBatchID < 1 || productBatchID > 99999999) {
-				return createResponse("error", 0, "ProduktBatch ID skal være mellem 1 - 99999999");
-			}
-			
-			if(raavareBatchID < 1 || raavareBatchID > 99999999) {
-				return createResponse("error", 0, "RåvareBatch ID skal være mellem 1 - 99999999");
-			}
-			
-			if(operatorID < 1 || operatorID > 99999999) {
-				return createResponse("error", 0, "Operatør ID skal være mellem 1 - 99999999");
-			}
-			
-			if(netto < 0.05 || netto > 20.0) {
-				return createResponse("error", 0, "Nettovægten skal være mellem 0.05 - 20.0");
-			}
-			
-			if(tara < 0.1 || tara > 10.0) {
-				return createResponse("error", 0, "Tara skal være mellem 0.1 - 10.0");
-			}
-			
-			try {
-				List<ProductBatchDTO> productBatches = mySQLController.getProductBatches();
-				List<IngBatchDTO> ingBatches = mySQLController.getIngBatches();
-				List<UserDTO> users = mySQLController.getUsers();
-				boolean productBatchFound = false;
-				boolean ingredientBatchFound = false;		
-				boolean userFound = false;
-				
-				if(mySQLController.getProductBatchComponent(productBatchID, raavareBatchID) != null) {
-					return createResponse("error", 0, "ProdukBatchkomponenten eksisterer allerede");
-				}
-				
-				for (ProductBatchDTO productBatch : productBatches) {
-					if(mySQLController.getProductBatch(productBatchID) != null) {
-						productBatchFound = true;
-					}
-				}	
-				for (IngBatchDTO ingBatch : ingBatches) {
-					if(mySQLController.getIngBatch(raavareBatchID) != null) {
-						ingredientBatchFound = true;
-					}
-				}
-				for (UserDTO user : users) {
-					if(mySQLController.getUser(operatorID) != null) {
-						userFound = true;
-					}
-				}
-				
-				if(productBatchFound && ingredientBatchFound && userFound) {					
-					mySQLController.createProductBatchComponent(productBatchID, raavareBatchID, operatorID, netto, tara);
-		
-				} else if (!productBatchFound) {
-					return createResponse("error", 0, "productBatchID eksistere ikke");
-				} else if (!ingredientBatchFound) {
-					return createResponse("error", 0, "raavareBatchID eksistere ikke");
-				} else if (!userFound) {
-					return createResponse("error", 0, "operatorID eksistere ikke");
-				}
-				
-				if(mySQLController.getProductBatchComponent(productBatchID, raavareBatchID) != null) {
-					ProductBatchComponentDTO createdProductBatchComponent = mySQLController.getProductBatchComponent(productBatchID, raavareBatchID);
-					return createResponse("success", 1, "ProduktBatchkomponenten med produktbatchen \"" + createdProductBatchComponent.getProductBatchID() + "\" blev oprettet");
-
-				} else {
-					return createResponse("error", 0, "Kunne ikke oprette produktbatchkomponenten");
-				}
-			} catch (SQLException e) {
-				return createResponse("error", e.getErrorCode(), e.getMessage());
-			}
+			return productBatchController.createProductBatchComponent(productBatchID, raavareBatchID, operatorID, netto, tara);
 		}
 		
 		@GET
 		@Path("getProductBatchComponent")
 		public String getProductBatchComponent(@QueryParam("productBatchID") int productBatchID, @QueryParam("raavareBatchID") int raavareBatchID) {
-			try {
-				return createResponse("success", 1, new Gson().toJson(mySQLController.getProductBatchComponent(productBatchID, raavareBatchID)));
-			} catch (SQLException e) {
-				return createResponse("error", e.getErrorCode(), e.getMessage());
-			}
-		
+			return productBatchController.getProductBatchComponent(productBatchID);
 		}
 		
 		//ProduktBatch-Liste
